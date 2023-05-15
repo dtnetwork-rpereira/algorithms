37816d6cfc40afd8903d3ad70202c388f07f5bd6
r.pereira <r.pereira@dtnetwork.com.br>
2023-05-15 17:04:49 -0300

    creating slice and merge

--- order/merge_sort/index.js
+++ order/merge_sort/index.js
@@ -1,5 +1,4 @@
 import { Guns } from '../guns.mjs';
-// const { Guns } = pkg;
 
 function mergeArrays(arrayOne, arrayTwo) {
   let newArray = [];
@@ -10,6 +9,9 @@ function mergeArrays(arrayOne, arrayTwo) {
   while (arrayOneIndex < arrayOne.length || arrayTwoIndex < arrayTwo.length) {
     const item1 = arrayOne[arrayOneIndex];
     const item2 = arrayTwo[arrayTwoIndex];
+
+    /* console.log('Array 1:', arrayOneIndex, arrayOne.length);
+    console.log('Array 2:', arrayTwoIndex, arrayTwo.length); */
 
     if (!item1) {
       newArray.push(item2);
@@ -37,10 +39,28 @@ function mergeArrays(arrayOne, arrayTwo) {
   return newArray;
 }
 
+function sliceArray(gunsArray) {
+  const slicedArrays = [];
+  let lowerIndex = 0;
+
+  for (let index = 0; index < gunsArray.length; index++) {
+    const currentItem = gunsArray[index];
+    const nextItem = gunsArray[index + 1];
+
+    if (!nextItem || currentItem.price > nextItem.price) {
+      slicedArrays.push(gunsArray.slice(lowerIndex, index + 1));
+      lowerIndex = index + 1;
+    }
+  }
+
+  return slicedArrays;
+}
+
 const gunGroupOne = new Guns();
 gunGroupOne.add({ name: '.38', price: 100 });
 gunGroupOne.add({ name: 'Bazuka', price: 150 });
 gunGroupOne.add({ name: 'AK-47', price: 300 });
+// gunGroupOne.add({ name: 'Minigun', price: 999 });
 
 const gunGroupTwo = new Guns();
 gunGroupTwo.add({ name: 'Shotgun', price: 50 });
@@ -48,4 +68,15 @@ gunGroupTwo.add({ name: 'Grenades (5x)', price: 250 });
 gunGroupTwo.add({ name: 'Shield', price: 259.99 });
 gunGroupTwo.add({ name: 'Krauser\'s Knife', price: 301 });
 
-console.log(mergeArrays(gunGroupOne.arsenal, gunGroupTwo.arsenal));
+// const gunGroupThree = new Guns();
+// gunGroupThree.add({ name: 'Common Knife', price: 67.05 });
+
+/* const arsenalMerged = mergeArrays(gunGroupOne.arsenal, gunGroupTwo.arsenal);
+
+for (const gun of arsenalMerged) {
+  console.log(`${gun.name}: $${gun.price}`);
+} */
+
+const slicedArrays = sliceArray([...gunGroupOne.arsenal, ...gunGroupTwo.arsenal]);
+
+console.log(mergeArrays(...slicedArrays));
